c0c7ce7fe1fed515fce8af7014a5ded46d815753
AveePB <mike303.py@gmail.com>
2024-04-20 13:50:15 +0200

    Clean setup

---
+++ fhs/src/test/java/dev/aveepb/fhs/FileHostingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.aveepb.fhs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileHostingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
